918aa0e50dbd4280f4b81d7598ee0142f7047ada
admin <admin@DESKTOP-98HRBAS>
2022-11-25 10:03:11 +0530

    Query Moved to Properites

--- spaceage-app/src/main/java/com/spaceage/service/impl/ItemMasterServiceImpl.java
+++ spaceage-app/src/main/java/com/spaceage/service/impl/ItemMasterServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -35,74 +36,53 @@ import com.spaceage.service.ItemMasterService;
 
 @Repository
 public class ItemMasterServiceImpl implements ItemMasterService {
-	 @Autowired
-	 private JdbcTemplate jdbcTemplate;
-	public final String INSER_BOM = "insert into tblbom (item_master_id,part_no,"
-			+"part_description,"
-			+"version,"
-			+"st_location,"
-			+"validity,"
-			+"cat_description,"
-			+"qty_required,"
-			+"qty_lot,"
-			+"primary_no,"
-			+"secondary_no,"
-			+"pack_code,"
-			+"pack_qty,"
-			+"packing_group,"
-			+"total_no_of_packing_group,"
-			+"mix_group,"
-			+"mix,"
-			+"bomNo,"
-			+"case_map,"
-			+"images) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-	
-	public final String INSER_ITEM = "insert into item_master(customer_code,"
-			+ "project_code,"
-			+ "org_country_id,"
-			+ "packing_type,"
-			+ "lot_size,"
-			+ "customer_login,"
-			+ "lot_ref_no,"
-			+ "containers) values(?,?,?,?,?,?,?,?)";
-	public void save(MultipartFile file, long itemId) {
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public void save(MultipartFile file, Integer itemId) {
 		try {
 			List<Bom> fileData = CSVHelper.csvToBom(file.getInputStream(), itemId);
 
-			jdbcTemplate.batchUpdate(INSER_BOM,new BatchPreparedStatementSetter() {
+			jdbcTemplate.batchUpdate(env.getProperty("insert_bom"), new BatchPreparedStatementSetter() {
 
 				@Override
 				public void setValues(PreparedStatement pStmt, int i) throws SQLException {
 					Bom bom = fileData.get(i);
-	                pStmt.setLong(1, bom.getItemMasterId());
-	                pStmt.setString(2, bom.getPartNo());
-	                pStmt.setString(3, bom.getPartDescription());
-	                pStmt.setString(4, bom.getVersion());
-	                pStmt.setString(5, bom.getStLoction());
-	                pStmt.setString(6, bom.getValidity());
-	                pStmt.setString(7, bom.getCatDescription());
-	                pStmt.setString(8, bom.getQtyRequired());
-					
-	                pStmt.setString(9, bom.getQtyLot());
-	                pStmt.setString(10, bom.getPrimaryNo());
-	                pStmt.setString(11, bom.getSecondaryNo());
-	                pStmt.setString(12, bom.getPackCode());
-	                
-	                pStmt.setString(13, bom.getPackQty());
-	                pStmt.setString(14, bom.getPackingGroup());
-	                pStmt.setString(15, bom.getTotalNoOfPackingGroup());
-	                pStmt.setString(16, bom.getMixGroup());
-	                
-	                pStmt.setString(17, bom.getMix());
-	                pStmt.setString(18, bom.getBomNo());
-	                pStmt.setString(19, bom.getCaseMap());
-	                pStmt.setString(20, bom.getImages());
+					pStmt.setLong(1, bom.getItemMasterId());
+					pStmt.setString(2, bom.getPartNo());
+					pStmt.setString(3, bom.getPartDescription());
+					pStmt.setString(4, bom.getVersion());
+					pStmt.setString(5, bom.getStLoction());
+					pStmt.setString(6, bom.getValidity());
+					pStmt.setString(7, bom.getCatDescription());
+					pStmt.setString(8, bom.getQtyRequired());
+
+					pStmt.setString(9, bom.getQtyLot());
+					pStmt.setString(10, bom.getPrimaryNo());
+					pStmt.setString(11, bom.getSecondaryNo());
+					pStmt.setString(12, bom.getPackCode());
+
+					pStmt.setString(13, bom.getPackQty());
+					pStmt.setString(14, bom.getPackingGroup());
+					pStmt.setString(15, bom.getTotalNoOfPackingGroup());
+					pStmt.setString(16, bom.getMixGroup());
+
+					pStmt.setString(17, bom.getMix());
+					pStmt.setString(18, bom.getBomNo());
+					pStmt.setString(19, bom.getCaseMap());
+					pStmt.setString(20, bom.getImages());
+					pStmt.setInt(21, 1);
 				}
 
 				@Override
 				public int getBatchSize() {
-					 return fileData.size();
-				}});
+					return fileData.size();
+				}
+			});
 		} catch (IOException e) {
 			throw new RuntimeException("fail to store csv data: " + e.getMessage());
 
@@ -112,16 +92,11 @@ public class ItemMasterServiceImpl implements ItemMasterService {
 		}
 	}
 
-	
-	
 	@Override
 	public List<Item_masterDTO> getAllItems() {
 		try {
-			String sql = "select i.item_id, i.lot_ref_no, c.customer_code, c.name, c.customer_location, p.project_code, p.project_name,  c.designation, i.createdDate, c.country from item_master i join project_master p ON i.project_code  = p.project_id join customer_master c ON i.customer_code  = c.customer_id";
-
-			List<Item_masterDTO> dto = jdbcTemplate.query(
-			        sql,
-			        new BeanPropertyRowMapper<Item_masterDTO>(Item_masterDTO.class));
+			List<Item_masterDTO> dto = jdbcTemplate.query(env.getProperty("select_all_item"),
+					new BeanPropertyRowMapper<Item_masterDTO>(Item_masterDTO.class));
 
 			return dto;
 		} catch (Exception e) {
@@ -133,35 +108,29 @@ public class ItemMasterServiceImpl implements ItemMasterService {
 	@Override
 	public List<Customer> getCustomer() {
 		try {
-			String sql = "select * from customer_master";
-
-			List<Customer> customers = jdbcTemplate.query(
-			        sql,
-			        new BeanPropertyRowMapper<Customer>(Customer.class));
+			List<Customer> customers = jdbcTemplate.query(env.getProperty("select_customer"),
+					new BeanPropertyRowMapper<Customer>(Customer.class));
 
 			return customers;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
-		
+
 	}
 
 	@Override
 	public List<Project> getProject() {
 		try {
-			String sql = "select * from project_master";
-
-			List<Project> project = jdbcTemplate.query(
-			        sql,
-			        new BeanPropertyRowMapper<Project>(Project.class));
+			List<Project> project = jdbcTemplate.query(env.getProperty("select_project"),
+					new BeanPropertyRowMapper<Project>(Project.class));
 
 			return project;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
-		
+
 	}
 
 	@Override
@@ -180,12 +149,13 @@ public class ItemMasterServiceImpl implements ItemMasterService {
 		return it;
 	}
 
-	public long save(Item itemJson) {
+	public Integer save(Item itemJson) {
 		KeyHolder holder = new GeneratedKeyHolder();
 		jdbcTemplate.update(new PreparedStatementCreator() {
 			@Override
 			public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-				PreparedStatement ps = connection.prepareStatement(INSER_ITEM, Statement.RETURN_GENERATED_KEYS);
+				PreparedStatement ps = connection.prepareStatement(env.getProperty("insert_item"),
+						Statement.RETURN_GENERATED_KEYS);
 				ps.setLong(1, itemJson.getCustomer_code());
 				ps.setLong(2, itemJson.getProject_code());
 				ps.setLong(3, itemJson.getOrg_country_id());
@@ -194,58 +164,52 @@ public class ItemMasterServiceImpl implements ItemMasterService {
 				ps.setLong(6, itemJson.getCustomer_login());
 				ps.setString(7, itemJson.getLot_ref_no());
 				ps.setString(8, itemJson.getContainers().toString());
+				ps.setInt(9, 1);
 				return ps;
 			}
 		}, holder);
-		return holder.getKey().intValue();
+
+		// holder.getKey().intValue();
+
+		return (Integer) holder.getKeyList().get(0).get("item_id");
 	}
-
-
 
 	@Override
 	public List<PackagingType> getPackagingType() {
 		try {
-			String sql = "select * from packaging_type";
-
-			List<PackagingType> type = jdbcTemplate.query(
-			        sql,
-			        new BeanPropertyRowMapper<PackagingType>(PackagingType.class));
+			List<PackagingType> type = jdbcTemplate.query(env.getProperty("select_packagingType"),
+					new BeanPropertyRowMapper<PackagingType>(PackagingType.class));
 
 			return type;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
-		
+
 	}
 
 	@Override
 	public List<Country> getCountry() {
 		try {
-			String sql = "select * from country";
-
-			List<Country> country = jdbcTemplate.query(
-			        sql,
-			        new BeanPropertyRowMapper<Country>(Country.class));
+			List<Country> country = jdbcTemplate.query(env.getProperty("select_country"),
+					new BeanPropertyRowMapper<Country>(Country.class));
 
 			return country;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
-		
+
 	}
-
-
 
 	@Override
 	public List<Bom> getBomById(String id) {
-		List<Bom> plist = new ArrayList<>(); 
+		List<Bom> plist = new ArrayList<>();
 		try {
-			String sql = "select * from tblbom where item_master_id=?";
-			List<Map<String, Object>> bomList = jdbcTemplate.queryForList(sql, id);
+			List<Map<String, Object>> bomList = jdbcTemplate.queryForList(env.getProperty("select_bom"),
+					Integer.parseInt(id));
 			bomList.forEach(m -> {
-				Bom b = new Bom();	
+				Bom b = new Bom();
 				b.setBomId((int) m.get("BOM_ID"));
 				b.setItemMasterId((int) m.get("item_master_id"));
 				b.setPartNo((String) m.get("PART_NO"));
@@ -273,7 +237,7 @@ public class ItemMasterServiceImpl implements ItemMasterService {
 				b.setModifiedDate((Date) m.get("ModifiedDate"));
 				b.setStatus((boolean) m.get("Status"));
 				plist.add(b);
-			}); 
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
